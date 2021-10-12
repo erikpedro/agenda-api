c2440882d715adf055826f02025d8b308fc2bed5
Erik Pedro <erikpedro@gmail.com>
2021-10-11 23:54:29 -0300

    CrossOrigin API Contatos

--- src/main/java/com/github/erikpedro/agendaapi/model/api/rest/ContatoController.java
+++ src/main/java/com/github/erikpedro/agendaapi/model/api/rest/ContatoController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
 
     private final ContatoRepository repository;
